35c8d70df69ff91f55bf496d72686d3a3767bd6d
Slipertit <pierlino.r@gmail.com>
2024-10-23 10:41:28 +0200

    Button

---
+++ src/Button.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+const ButtonUsage: React.FC = () => {
+  return <Button variant="contained"> Click me plz </Button>;
+}
+
+export default ButtonUsage;
