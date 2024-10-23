6be3f7b2260644efe9d494c53f59e0e6116436df
Simon <simsimland2004@gmail.com>
2024-10-23 11:22:38 +0200

    update 23/10-11:22

--- src/Content/RecordComponent.tsx
+++ src/Content/RecordComponent.tsx
@@ -1,20 +1,29 @@
-import { colors } from "@mui/material";
 import Button from "@mui/material/Button";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const RecordComponent: React.FC = () => {
+  const [recordIsDisabled, SetRecordIsDisabled] = useState(false);
+  const [stopIsDisabled, SetStopIsDisabled] = useState(true);
   useEffect(() => {
-    const record = document.querySelector("#record") as HTMLElement;
-    const stop = document.querySelector("#stop") as HTMLElement;
+    const record = document.querySelector("#record") as HTMLButtonElement;
+    const stop = document.querySelector("#stop") as HTMLButtonElement;
     const soundClips = document.querySelector("#sound-clips") as HTMLElement;
 
     if (!record || !stop || !soundClips) {
-        console.log("Les éléments #record, #stop ou #sound-clips ne sont pas encore disponibles.");
-        return;
-      }
+      console.log(
+        "Les éléments #record, #stop ou #sound-clips ne sont pas encore disponibles."
+      );
+      return;
+    }
 
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia && record != null && stop != null && soundClips != null) {
-        //Garder la condition comme tel sinon Typescript boude
+    if (
+      navigator.mediaDevices &&
+      navigator.mediaDevices.getUserMedia &&
+      record != null &&
+      stop != null &&
+      soundClips != null
+    ) {
+      //Garder la condition comme tel sinon Typescript boude
       console.log("getUserMedia supported.");
       navigator.mediaDevices
         // On veut que l'audio
@@ -27,8 +36,55 @@ const RecordComponent: React.FC = () => {
             mediaRecorder.start();
             console.log(mediaRecorder.state);
             console.log("recorder started");
+            SetRecordIsDisabled(true);
+            SetStopIsDisabled(false);
           };
-          
+
+          let chunks: any = [];
+
+          mediaRecorder.ondataavailable = (e) => {
+            chunks.push(e.data);
+          };
+
+          stop.onclick = () => {
+            mediaRecorder.stop();
+            console.log(mediaRecorder.state);
+            console.log("recorder stopped");
+            SetRecordIsDisabled(false);
+            SetStopIsDisabled(true);
+          };
+
+          mediaRecorder.onstop = (e) => {
+            console.log("recorder stopped");
+
+            const clipName = prompt("Enter a name for your sound clip");
+
+            const clipContainer = document.createElement("article");
+            const clipLabel = document.createElement("p");
+            const audio = document.createElement("audio");
+            const deleteButton = document.createElement("button");
+
+            clipContainer.classList.add("clip");
+            audio.setAttribute("controls", "");
+            deleteButton.textContent = "Delete";
+            clipLabel.textContent = clipName;
+
+            clipContainer.appendChild(audio);
+            clipContainer.appendChild(clipLabel);
+            clipContainer.appendChild(deleteButton);
+            soundClips.appendChild(clipContainer);
+
+            const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+            chunks = [];
+            const audioURL = window.URL.createObjectURL(blob);
+            audio.src = audioURL;
+
+            deleteButton.onclick = (e) => {
+              let evtTgt = e.target as Element;
+              (evtTgt!.parentNode!.parentNode as HTMLElement).removeChild(evtTgt!.parentNode as HTMLElement);
+
+            };
+          };
         })
         .catch((err) => {
           console.error(`The following getUserMedia error occurred: ${err}`);
@@ -38,13 +94,16 @@ const RecordComponent: React.FC = () => {
     }
   });
 
-  console.log("ici");
-
   return (
-    <article style={{color:"#FFFFFF"}}>
+    <article className="clip" style={{ color: "#FFFFFF" }}>
       <p>Component</p>
-      <Button id="record" variant="contained">Rec</Button>
-      <Button id="stop" variant="contained" disabled>Stop</Button>
+      <audio controls></audio>
+      <Button id="record" variant="contained" disabled={recordIsDisabled}>
+        Rec
+      </Button>
+      <Button id="stop" variant="contained" disabled={stopIsDisabled}>
+        Stop
+      </Button>
       <div id="sound-clips">Clip</div>
     </article>
   );
