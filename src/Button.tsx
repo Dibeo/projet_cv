c9d2ef2427a07c136e8f7c30620a7d188e8bfd2f
Slipertit <pierlino.r@gmail.com>
2024-10-23 11:01:43 +0200

    Button with counter, I'm a professional

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,8 +1,16 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 const ButtonUsage: React.FC = () => {
-  return <Button variant="contained"> Click me plz </Button>;
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    console.log("Hello World");
+    setCount(count+1);
+  }
+
+  return <Button variant="contained" onClick={handleClick}> Click me plz (Clicked {count} times) </Button>;
 }
 
 export default ButtonUsage;
