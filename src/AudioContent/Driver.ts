ac638025bf94dcbc29da6a279d9aadbf2450156e
Dibeo <115413042+Dibeo@users.noreply.github.com>
2024-10-27 16:03:20 +0100

    + de francais

--- src/AudioContent/Driver.ts
+++ src/AudioContent/Driver.ts
@@ -3,13 +3,17 @@ import "driver.js/dist/driver.css";
 
 const driverObj = driver({
   showProgress: true,
+  nextBtnText: 'Suivant',
+  prevBtnText: 'Precedent',
+  doneBtnText: 'Terminer',
   steps: [
-    { element: '#record', popover: { title: 'Record', description: 'Pour lancer l\'enregistrement.' } },
-    { element: '#stop', popover: { title: 'Stop', description: 'Pour arreter d\'enregistrer.' } },
+    { element: '#record', popover: { title: 'Enregistrer', description: 'Pour lancer l\'enregistrement.' } },
+    { element: '#stop', popover: { title: 'Arrêter', description: 'Pour arrêter d\'enregistrer.' } },
     { element: '#Exemple', popover: { title: 'Résultat', description: 'Ici se trouve votre enregistrement (celui-ci est un exemple).' } },
-    { element: '#download', popover: { title: 'Téléchargement', description: 'Ici vous pouvez télécharger votre CV.' } },
-    { element: '#delete', popover: { title: 'Résultat', description: 'Pour supprimer votre CV.' } },
-    { element: '#select', popover: { title: 'Résultat', description: 'Pour selectionner le cv que vous souhaiter utiliser pour les étapes suivantes.' } },
+    { element: '#download', popover: { title: 'Télécharger', description: 'Ici vous pouvez télécharger votre CV.' } },
+    { element: '#delete', popover: { title: 'Supprimer', description: 'Pour supprimer votre CV.' } },
+    { element: '#select', popover: { title: 'Sélectionner', description: 'Pour sélectionner le CV que vous souhaitez utiliser pour les étapes suivantes.' } },
+    { popover: { title: 'À votre tour', description: 'Vous avez maintenant les informations importantes pour démarrer votre enregistrement.' } }
   ]
 });
 
