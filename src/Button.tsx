c768e17ffa5e93c28599f44a624998b92a943bce
Slipertit <pierlino.r@gmail.com>
2024-10-28 15:31:14 +0100

    tests react

--- src/Button.tsx
+++ src/Button.tsx
@@ -3,14 +3,16 @@ import Button from '@mui/material/Button';
 import { useState } from 'react';
 
 const ButtonUsage: React.FC = () => {
-  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(0);
 
-  function handleClick() {
+  function handleClick1() {
     console.log("Hello World");
-    setCount(count+1);
+    setCount1(count1+1);
   }
 
-  return <Button variant="contained" onClick={handleClick}> Click me plz (Clicked {count} times) </Button>;
+  return (
+    <Button variant="contained" onClick={handleClick1}> Click me plz (Clicked {count1} times) </Button>;
+    <Button variant="contained" onClick={handleClick1}> Or click me (Clicked {count1} times) </Button>;)
 }
 
 export default ButtonUsage;
