4004a3eb14f25999415eb2f0a43aa01be84fa309
Simon <simsimland2004@gmail.com>
2024-11-27 10:13:37 +0100

    - eslint

--- Server/ts/database.ts
+++ Server/ts/database.ts
@@ -1,10 +1,22 @@
 import sqlite3 from "sqlite3";
 
 const databaseGest = () => {
-  const db = new sqlite3.Database(
-    "database/result_c.db",
-    sqlite3.OPEN_READWRITE
+  const db : sqlite3.Database = new sqlite3.Database(
+    "database/result_cv.db"
   );
+
+  let x:any;
+
+  const execute = async (db : sqlite3.Database, sql) => {
+    return new Promise((resolve, reject) => {
+      db.exec(sql, (err) => {
+        if (err) reject(err);
+        resolve(x);
+      });
+    });
+  };
+
+  execute(db, "")
 };
 
 export default databaseGest;
